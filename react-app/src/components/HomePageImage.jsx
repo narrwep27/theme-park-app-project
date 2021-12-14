2060006ae23878d8ed56a4d8065a5c22039642b4
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-14 10:51:35 -0800

    Added initial code to Home and HomePageImage

---
+++ react-app/src/components/HomePageImage.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function HomePageImage() {
+  return <div className="homepage-image"></div>
+};
--- react-app/src/routes/Home.jsx
+++ react-app/src/routes/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header';
+import HomePageImage from '../components/HomePageImage';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <div>
-      
+      <Header />
+      <HomePageImage />
+      <Footer />
     </div>
   )
 };
