e6e08f2bd25411302ff72341f71cb3b3ac2b4609
menthus123 <MarvelJohnson@Hotmail.com>
2021-12-13 16:24:35 -0800

    Set up react routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Rides from './routes/Rides';
+import Tickets from './routes/Tickets';
+import Entry from './routes/Entry';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/entry" component={Rides} />
+        <Route path="/rides" render={Tickets} />
+        <Route path="/tickets" component={Entry} />
       </Switch>
     </div>
   );
