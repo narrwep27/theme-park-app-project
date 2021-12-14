307f30298d1d7ad4c66cf9c5f9183795687fca8d
Narrwe <narrwep@gmail.com>
2021-12-14 14:09:04 -0500

    added extra documents to rides collection and credited on ReadMe

--- back-end/seed/rides.js
+++ back-end/seed/rides.js
@@ -10,35 +10,70 @@ const main = async () => {
             thrillLevel: '10',
             minimumHeight: '48 in',
             description: 'Unbelievable intensity on our fastest roller coaster yet!',
-            imageUrl: 'https://www.sixflags.com/sites/default/files/2017_sfne_superman_7.png'
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fsfneonline.org%2Fwp-content%2Fuploads%2F2015%2F09%2FSuperman-The-Ride-Key-Art.jpg&f=1&nofb=1'
         },
         {
             name: 'Flight of Fear',
             thrillLevel: '8',
             minimumHeight: '48 in',
             description: `Our fastest indoor roller coaster! Don't blink or you might miss all the hidden surprises that lurks around...`,
-            imageUrl: 'https://th.bing.com/th/id/R.3619bb43ee45873e889829763b7b73c7?rik=KrqvdGNR3d16JQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-hI1LQhl_LJ8%2fUc34p2Kn5HI%2fAAAAAAAAOMU%2f80fR97pQSJY%2fs1600%2fki70.jpg&ehk=dO7aqWP5VIrR8et0s2LjQBSl27mTAF19RFmrt6ZmL4c%3d&risl=&pid=ImgRaw&r=0'
+            imageUrl: 'https://i.ytimg.com/vi/iHOp3pvuStk/maxresdefault.jpg'
         },
         {
             name: 'Teacups',
             thrillLevel: '3',
             minimumHeight: '30 in',
             description: "The classic teacups! Dive in and swirl around with your friends!",
-            imageUrl: 'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555309375/shape/mentalfloss/teacups_primary.jpg?itok=m6HO15-m'
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages2.minutemediacdn.com%2Fimage%2Fupload%2Fc_fill%2Cg_auto%2Ch_1248%2Cw_2220%2Fv1555309375%2Fshape%2Fmentalfloss%2Fteacups_primary.jpg%3Fitok%3Dm6HO15-m&f=1&nofb=1'
         },
         {
             name: 'Bumper Cars',
-            thrillLevel: '3',
+            thrillLevel: '4',
             minimumHeight: '30 in',
             description: 'The rule is to buckle up and drive UNsafe! Control your car as you collid with others in this classic attraction!',
-            imageUrl: 'https://th.bing.com/th/id/R.bf1f031025ad17030a29ef3ecf12c345?rik=6BAIrQptO5XNNg&pid=ImgRaw&r=0&sres=1&sresct=1'
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Famusementrides.org%2Fwp-content%2Fuploads%2F2016%2F03%2FThe-Best-Indoor-Bumper-Cars-For-Sale.jpg&f=1&nofb=1'
         },
         {
             name: 'G-Force',
             thrillLevel: '9',
             minimumHeight: '48 in',
             description: 'Hold onto your seats as you drop from 400 feet in the air!',
-            imageUrl: 'https://th.bing.com/th/id/OIP.ViqJ_3B1XSu31x_oGNJFfwHaJ3?pid=ImgDet&rs=1'
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faboutthemeparks.net%2Fwp-content%2Fuploads%2F2019%2F03%2FKings_Dominion-Drop-Tower-1024x683.jpg&f=1&nofb=1'
+        },
+        {
+            name: 'Merry Go Round',
+            thrillLevel: '2',
+            minimumHeight: '30 in',
+            description: 'Choose a steed and wave to family and friends!',
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fih1.redbubble.net%2Fimage.5992760.7776%2Fflat%2C1000x1000%2C075%2Cf.u2.jpg&f=1&nofb=1'
+        },
+        {
+            name: 'Swing Carousel',
+            thrillLevel: '5',
+            minimumHeight: '30 in',
+            description: 'Grab a seat and fly through the air as the carousel spins!',
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Fcolorful-chain-swing-carousel-motion-amusement-park-blue-sky-background-96947745.jpg&f=1&nofb=1'
+        },
+        {
+            name: 'Pirate Ship',
+            thrillLevel: '7',
+            minimumHeight: '48 in',
+            description: 'Swing back and forth between on a pirate ship and hold on tight!',
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fsimplotel%2Fimage%2Fupload%2Fx_0%2Cy_120%2Cw_1280%2Ch_720%2Cr_0%2Cc_crop%2Cq_80%2Cfl_progressive%2Fw_960%2Cf_auto%2Cc_fit%2Fwonderla-amusement-park-bangalore%2Fdry_rides_pirate_ship_1_wonderla_amusement_parks_bangalore_k1fegu&f=1&nofb=1'
+        },
+        {
+            name: 'Wild Mouse',
+            thrillLevel: '7',
+            minimumHeight: '48 in',
+            description: `Roller coasters don't have to be fast. Get swung around in the twists and turns of Wild Mouse!`,
+            imageUrl: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcoasterforce.com%2Fdev%2Fwp-content%2Fuploads%2F2016%2F10%2FWildMouseCasinoPier-1.jpg&f=1&nofb=1'
+        },
+        {
+            name: 'Ferris Wheel',
+            thrillLevel: '4',
+            minimumHeight: '30 in',
+            description: 'Get a beautiful view across the amusement park at the peak of our ferris wheel!',
+            imageUrl: 'https://c.wallhere.com/photos/4c/51/ferris_wheel_oktoberfest_entertainment_attraction-554646.jpg!d'
         }
     ]
     
