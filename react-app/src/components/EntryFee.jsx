6c837bc4d348a4a2190011742450d162504db8e6
Narrwe <narrwep@gmail.com>
2021-12-14 11:26:45 -0500

    entryfee comp and entry route formatted and prepared

--- react-app/src/components/EntryFee.jsx
+++ react-app/src/components/EntryFee.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function EntryFee() {
+export default function EntryFee(props) {
   return (
-    <div>
-      
+    <div className='entryfee'>
+      <h3>Ages {props.age} and under: ${props.fee}</h3>
     </div>
   );
 };
